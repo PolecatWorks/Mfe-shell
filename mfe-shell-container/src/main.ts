3622f66e8442c2524097bac71705e6a976f7658d
Ben Greene <BenJGreene@gmail.com>
2026-02-10 00:14:55 +0000

    MFE load from json

--- mfe-shell-container/src/main.ts
+++ mfe-shell-container/src/main.ts
@@ -1,8 +1,12 @@
 import { initFederation } from '@angular-architects/native-federation';
 
+
 fetch('/assets/contents/mfe.json')
-  .then((res) => res.json())
-  .then((config) => initFederation(config))
-  .catch((err) => console.error(err))
-  .then((_) => import('./bootstrap'))
-  .catch((err) => console.error(err));
+  .then(res => res.json())
+  .then(config => {
+    console.log("Loading MFE as: ", config)
+    return initFederation(config)
+  })
+  .catch(err => console.error(err))
+  .then(_ => import('./bootstrap'))
+  .catch(err => console.error(err))
