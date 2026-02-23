2059c427263d7eef7979094782d681c653054811
Ben Greene <BenJGreene@gmail.com>
2026-02-23 22:58:46 +0000

    fix(mfe1): correctly expose remoteRoutes and use loadChildren in shell to support nested routing

--- mfe1-container/src/app/app.routes.ts
+++ mfe1-container/src/app/app.routes.ts
@@ -2,9 +2,18 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Chooser } from './chooser/chooser';
 import { Viewer } from './viewer/viewer';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'chooser', component: Chooser },
     { path: 'viewer', component: Viewer }
+];
+
+export const remoteRoutes: Routes = [
+    {
+        path: '',
+        component: AppComponent,
+        children: routes
+    }
 ];
