a0f37ecca03c9a49bd57e6344942f4f20326fafe
Ben Greene <BenJGreene@gmail.com>
2026-02-25 22:43:53 +0000

    task: Move sidebar navigation to top navbar dropdown

--- mfe-shell-container/src/app/components/home/home.component.ts
+++ mfe-shell-container/src/app/components/home/home.component.ts
@@ -3,8 +3,7 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { AsyncPipe } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -15,7 +14,7 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatSidenavModule, MatListModule, MatIconModule, AsyncPipe, RouterOutlet, RouterLink],
+  imports: [MatToolbarModule, MatButtonModule, MatMenuModule, MatIconModule, AsyncPipe, RouterOutlet, RouterLink],
 })
 export class HomeComponent {
   private breakpointObserver = inject(BreakpointObserver);
