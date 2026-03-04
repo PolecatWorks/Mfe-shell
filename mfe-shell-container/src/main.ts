f75b4c0f01276bc68087be58f8b59b7ad5e279ef
Ben Greene <BenJGreene@gmail.com>
2026-03-04 18:42:22 +0000

    task: rename mfe.json and implement dynamic shell menu

--- mfe-shell-container/src/main.ts
+++ mfe-shell-container/src/main.ts
@@ -1,11 +1,11 @@
 import { initFederation } from '@angular-architects/native-federation';
 
 
-fetch('/assets/contents/mfe.json')
+fetch('/assets/contents/shell-config.json')
   .then(res => res.json())
   .then(config => {
-    console.log("Loading MFE as: ", config)
-    return initFederation(config)
+    console.log("Loading MFE config: ", config)
+    return initFederation(config.mfes)
   })
   .catch(err => console.error(err))
   .then(_ => import('./bootstrap'))
