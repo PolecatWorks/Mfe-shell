517e287f522fac9aac8d7b4b3f739b32b62f0bea
Ben Greene <BenJGreene@gmail.com>
2026-03-07 14:19:52 +0000

    task: fix merge conflicts in mfe-shell-container to fix docker build

--- mfe-shell-container/src/main.ts
+++ mfe-shell-container/src/main.ts
@@ -4,14 +4,9 @@ import { initFederation } from '@angular-architects/native-federation';
 fetch('/assets/contents/shell-config.json')
   .then(res => res.json())
   .then(config => {
-<<<<<<< HEAD
     console.log("Loading MFE config: ", config)
-    return initFederation(config.mfes)
-=======
-    console.log("Loading MFE as: ", config)
     return initFederation(config.remotes)
       .then(() => config);
->>>>>>> 39d378d (Make MFE Shell configurable via JSON)
   })
   .catch(err => console.error(err))
   .then(config => import('./bootstrap').then(m => m.default(config)))
