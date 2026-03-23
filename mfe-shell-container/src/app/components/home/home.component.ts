18753c4695d44742de1cef4184ae67d2ec4ca84b
Ben Greene <BenJGreene@gmail.com>
2026-03-23 22:30:22 +0000

    task: implement navigation rail to save top bar space

--- mfe-shell-container/src/app/components/home/home.component.ts
+++ mfe-shell-container/src/app/components/home/home.component.ts
@@ -1,14 +1,15 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { AsyncPipe, CommonModule } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MFE_CONFIG, MenuConfig } from '../../mfe-config';
 import { fadeAnimation } from '../../route-animations';
 
@@ -17,7 +18,7 @@ import { fadeAnimation } from '../../route-animations';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatMenuModule, MatIconModule, MatTooltipModule, RouterOutlet, RouterLink, CommonModule],
+  imports: [MatSidenavModule, MatListModule, MatButtonModule, MatMenuModule, MatIconModule, MatTooltipModule, RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   animations: [fadeAnimation]
 })
 export class HomeComponent implements OnInit {
@@ -35,6 +36,10 @@ export class HomeComponent implements OnInit {
     if (this.config && this.config.menu) {
       this.menuItems = this.config.menu;
     }
+  }
+
+  reload() {
+    window.location.reload();
   }
 
   getRouteAnimationData(outlet: RouterOutlet) {
