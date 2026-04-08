871ff82fa69e8b480ab0bee2f7712376563d73d8
Ben Greene <BenJGreene@gmail.com>
2026-04-08 22:37:29 +0100

    fix: resolve DataShow and PersonalDataForm mapping issues in MFE wrappers

--- mfe1-container/src/app/personal-data-form/personal-data-form-wrapper.ts
+++ mfe1-container/src/app/personal-data-form/personal-data-form-wrapper.ts
@@ -22,19 +22,27 @@ export async function mount(container: HTMLElement, props: any) {
   });
 
   // Set the inputs from props
-  if (props) {
+  if (props && props.content) {
+    const data = props.content;
+    if (data.first_name) componentRef.setInput('firstName', data.first_name);
+    if (data.last_name) componentRef.setInput('lastName', data.last_name);
+    if (data.email) componentRef.setInput('email', data.email);
+    if (data.phone_number) componentRef.setInput('phoneNumber', data.phone_number);
+    if (data.address) componentRef.setInput('address', data.address);
+    if (data.actions) componentRef.setInput('actions', data.actions);
+  } else if (props) {
     if (props.firstName) componentRef.setInput('firstName', props.firstName);
     if (props.lastName) componentRef.setInput('lastName', props.lastName);
     if (props.email) componentRef.setInput('email', props.email);
     if (props.phoneNumber) componentRef.setInput('phoneNumber', props.phoneNumber);
     if (props.address) componentRef.setInput('address', props.address);
     if (props.actions) componentRef.setInput('actions', props.actions);
+  }
 
-    if (props.onAction) {
-      componentRef.instance.actionEvent.subscribe((payload: any) => {
-        props.onAction('submit', payload);
-      });
-    }
+  if (props && props.onAction) {
+    componentRef.instance.actionEvent.subscribe((payload: any) => {
+      props.onAction('submit', payload);
+    });
   }
 
   // Attach the component view to the ApplicationRef
